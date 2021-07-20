bb2ffc823c9f016bb1d55751ebac1d6a4496d926
Anja Laube <mail@anjalaube.com>
2021-07-20 14:39:58 +0200

    just for pulling purposes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -88,36 +88,3 @@ var Counter = function (_React$Component) {
 
 ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
 
-// let count = 0
-// const addOne = () => {
-//   count++
-//   renderCounterApp()
-// }
-
-// const subOne = () => {
-//   console.log("substract one")
-//   count--
-//   renderCounterApp()
-// }
-
-// const reset = () => {
-//   count = 0
-//   renderCounterApp()
-// }
-
-// const appRoot = document.getElementById('app');
-
-// const renderCounterApp = () => {
-//   const templateTwo = (
-//     <div>
-//       <h1> Count: {count}</h1>
-//       <button id='plus-one' onClick={addOne}>+1</button>
-//       <button onClick={subOne}>-1</button>
-//       <button onClick={reset}>reset</button>
-//     </div>
-//   )
-
-//   ReactDOM.render(templateTwo, appRoot)
-// }
-
-// renderCounterApp()
